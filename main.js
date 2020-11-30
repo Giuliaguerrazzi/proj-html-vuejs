04b76bdc78ee6ca1ec4c8a27e1aad089b9e4328a
Giulia <giulia.guerrazzi@live.it>
2020-11-30 14:34:24 +0100

    navbar icon

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 const app = new Vue ({
     el: '#app',
+    data: {
+        icons: [
+            {
+                link: 'home',
+                name: 'Home'
+            },
+            {
+                link: 'document',
+                name: 'Pages',
+            },
+            {
+                link: 'printer',
+                name: 'Blog',
+            },
+            {
+                link: 'cart',
+                name: 'Shop',
+            },
+            {
+                link: 'lab',
+                name: 'Shortcodes'
+            },
+            {
+                link: 'chat',
+                name: 'Support'
+            },
+            {
+                link: 'envelope',
+                name: 'Contact'
+            },
+        ]
+    }
 });
