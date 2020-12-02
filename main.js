dec11c288805b79e07d10a1ec123e323696c0f60
Giulia <giulia.guerrazzi@live.it>
2020-12-02 21:56:00 +0100

    add footer

--- main.js
+++ main.js
@@ -50,8 +50,46 @@ const app = new Vue ({
             },
             {
                 month: 'MAY 2014(1)'
+            },            
+        ],
+
+        features: [
+            {
+                info: 'Friendly and welcoming place'
             },
-            
+            {
+                info: 'Science and art classes'
+            },
+            {
+                info: 'Positive learning environment'
+            },
+            {
+                info: 'Educational field trips'
+            },
+            {
+                info: 'Writing and reading classes'
+            },
+            {
+                info: 'Science and art classes'
+            },
+        ],
+
+        posts: [
+            {
+                image: '_09-150x150',
+                lesson: 'Drawing and painting',
+                data: 'October 3, 2014'
+            },
+            {
+                image: '_10-150x150',
+                lesson: 'Fall parents meeting day',
+                data: 'October 3, 2014'
+            },
+            {
+                image: '_08-150x150',
+                lesson: 'Birthday in Kindergarten',
+                data: 'September 20, 2014'
+            },
         ]
     }
 });
